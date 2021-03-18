a58c3c579121e87597340dcaed9dbd381b2a0adb
Sefi Cohen <seficohen1@gmail.com>
2021-03-18 18:37:14 +0200

    added and config database

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : `${process.env.PORT}`,
     user : 'seficohen',
     password : '',
     database : 'smart-brain'
